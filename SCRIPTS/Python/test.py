6a111ed7eece57de50a98655606299f32e5d1609
Roger <roger@rogerperkin.co.uk>
2019-02-27 09:25:51 -0800

    updated ip

--- SCRIPTS/Python/test.py
+++ SCRIPTS/Python/test.py
@@ -7,11 +7,11 @@ iosv_l2 = {
     'password': 'cisco'
 }
 
-net_connect = ConnectHandler(**iosv_l2)
+net_connect = ConnectHandler
 output = net_connect.send_command('show ip int brief')
 print (output)
 
-config_commands = ['int loop 0', 'ip address 1.1.1.1 255.255.255.0']
+config_commands = ['int loop 2', 'ip address 2.2.2.2 255.255.255.0']
 output = net_connect.send_config_set(config_commands)
 print (output)
 
