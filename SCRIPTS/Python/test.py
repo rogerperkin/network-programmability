eac86637263a5d940e7371488304029915a25697
Roger <roger@rogerperkin.co.uk>
2019-02-27 09:21:30 -0800

    first python script

--- SCRIPTS/Python/test.py
+++ SCRIPTS/Python/test.py
@@ -1,1 +1,17 @@
-test
+from netmiko import ConnectHandler
+
+iosv_l2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.1.210',
+    'username': 'roger',
+    'password': 'cisco'
+}
+
+net_connect = ConnectHandler(**iosv_l2)
+output = net_connect.send_command('show ip int brief')
+print (output)
+
+config_commands = ['int loop 0', 'ip address 1.1.1.1 255.255.255.0']
+output = net_connect.send_config_set(config_commands)
+print (output)
+
