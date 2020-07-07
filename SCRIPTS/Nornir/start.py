61a4f8ca39738f094859840406925d7575d16933
Roger <roger@rogerperkin.co.uk>
2020-07-07 21:45:01 +0100

    updated workers

--- SCRIPTS/Nornir/start.py
+++ SCRIPTS/Nornir/start.py
@@ -1,11 +1,13 @@
 from nornir import InitNornir
+from nornir.core.filter import F
 
 nr = InitNornir("config.yml")
 
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.functions.text import print_result
 
-devices = nr.filter (groups="CSR-Router")
-result = devices.run(netmiko_send_command, command_string="sh int ip brief")
+devices = nr.filter((F(hostname__contains="220")))
+
+result = devices.run(netmiko_send_command, command_string="sh ip int brief")
 
 print_result(result)
