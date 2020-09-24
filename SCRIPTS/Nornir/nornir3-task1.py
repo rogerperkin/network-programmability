d731ce44c59c5da3cfb718de0e7f2c19b3690b06
Roger <roger@rogerperkin.co.uk>
2020-09-24 09:47:24 -0400

    First task for Nornir 3.0 testing

---
+++ SCRIPTS/Nornir/nornir3-task1.py
@@ -0,0 +1,15 @@
+from nornir import InitNornir
+
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko import netmiko_send_command
+
+from nornir.core.filter import F
+
+nr = InitNornir(config_file="config.yml")
+
+Router = nr.filter(F(groups__contains="CSR"))
+
+result = Router.run(netmiko_send_command, command_string="show ip int brief")
+
+print_result(result)
+
