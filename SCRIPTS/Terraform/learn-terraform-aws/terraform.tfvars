5eacc8b83b5d39a04756e390caab7bc46b2eb8ff
Roger <roger@rogerperkin.co.uk>
2021-05-20 08:39:54 -0700

    clear terraform aws files

--- SCRIPTS/Terraform/learn-terraform-aws/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-aws_access_key   = "sdfasd"
-aws_secret_key   = "sdfasdf"
-key_name         = ""
-private_key_path = ""
-
--- SCRIPTS/Terraform/learn-terraform-aws/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
-variable "aws_region" {
-  default = "eu-west-2"
-}
