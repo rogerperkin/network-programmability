7d694ef3c5b47d579718a6e5f7b7c5eefb472fb3
Roger <roger@rogerperkin.co.uk>
2021-05-20 08:27:48 -0700

    Terraform updates

---
+++ SCRIPTS/Terraform/learn-terraform-aws/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_access_key   = "sdfasd"
+aws_secret_key   = "sdfasdf"
+key_name         = ""
+private_key_path = ""
+
---
+++ SCRIPTS/Terraform/learn-terraform-aws/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "eu-west-2"
+}
